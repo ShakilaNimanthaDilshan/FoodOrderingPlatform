10f6d295202d33d07c1eaf94e54893fdf67cbea6
ShakilaNimanthaDilshan <ict22063@std.uwu.ac.lk>
2025-05-21 18:15:39 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv").config()
+const connectDb = require("./config/connectionDB")
 
 const PORT = process.env.PORT || 3000
+connectDb()
+app.use(express.json())
 
-app.get("/", (req,res)=>{
-    res.json({message:"hello"})
-})
+app.use("/recipe",require("./routes/recipe"))
+
 app.listen(PORT,(err)=>{
     console.log(`app is listening on port ${PORT} `)
 })
